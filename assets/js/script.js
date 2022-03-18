b793bb2820b072d441501acff6570c48de6bfa3b
Brittany Ganser <15brganser@gmail.com>
2022-03-17 23:08:19 -0600

    ability to search for parks by state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 //nps api key P7v76VxhVDmo5rOLwAyEnDqiIYeclDPZgcT0CdBK
+var searchState;
 
-var nationalParksUrl =
-  "https://developer.nps.gov/api/v1/parks?stateCode=NM&stateCode=&limit=20&start=0&sort=&api_key=P7v76VxhVDmo5rOLwAyEnDqiIYeclDPZgcT0CdBK";
+var getPark = function (searchTerm) {
+  var nationalParksUrl =
+    "https://developer.nps.gov/api/v1/parks?stateCode=" +
+    searchState +
+    "&stateCode=&limit=20&start=0&sort=&api_key=P7v76VxhVDmo5rOLwAyEnDqiIYeclDPZgcT0CdBK";
+  fetch(nationalParksUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+};
 
-fetch(nationalParksUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
+var displayParks = function () {
+  $(".searchBtn").on("click", function () {
+    searchState = $("#state").val();
+    console.log(searchState);
+    getPark();
   });
+};
+displayParks();
