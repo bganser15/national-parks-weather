6733a4a4e3d8e5ace8bcbe0f611e98a698681fda
Jaime Rivas <j.rivas4818@gmail.com>
2022-03-18 12:06:40 -0700

    pulled weather api data to display in js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+const API_KEY = "efce923fdeaa8b3017a1d2da22a5ca95";
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall';
+const defaultCoords = { lat: 33.625274, lon: -112.218690 };
+
 //nps api key P7v76VxhVDmo5rOLwAyEnDqiIYeclDPZgcT0CdBK
 var searchState;
 
@@ -23,3 +27,30 @@ var displayParks = function () {
   });
 };
 displayParks();
+
+
+$(document).ready(() => {
+  // Initial data fetch
+  fetchWeather();
+})
+
+// easy way to reuse and build the url
+/**
+* 
+* @param lat: latitude desired - default from defaultCoords
+* @param long: longitude desired - default from defaultCoords
+* @returns Promise<whatever the data is>
+*/
+const getUrl = (lat, lon) => {
+  const urlLat = lat || defaultCoords.lat;
+  const urlLon = lon || defaultCoords.lon
+  return `${BASE_URL}?units=imperial&lat=${urlLat}&lon=${urlLon}&appid=${API_KEY}`;
+}
+
+const fetchWeather = async (lat, lon) => {
+  return await fetch(getUrl())
+      .then((res) => res.json())
+      .then((res) => $('.data-json').append(JSON.stringify(res, null, 2)))
+      .catch((err) => console.error('WEATHER ERR: ', err));
+};
+
