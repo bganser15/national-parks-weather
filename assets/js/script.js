35a472e6868290870d622f3705e452ac62346339
Brittany Ganser <15brganser@gmail.com>
2022-03-22 20:06:47 -0600

    accesing images thru nps api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const API_KEY = "efce923fdeaa8b3017a1d2da22a5ca95";
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall';
-const defaultCoords = { lat: 33.625274, lon: -112.218690 };
+const BASE_URL = "https://api.openweathermap.org/data/2.5/onecall";
+const defaultCoords = { lat: 33.625274, lon: -112.21869 };
 
 //nps api key P7v76VxhVDmo5rOLwAyEnDqiIYeclDPZgcT0CdBK
 var searchState;
@@ -15,7 +15,10 @@ var getPark = function (searchTerm) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      console.log(data.data[0]);
+      var imageUrl = data.data[0].images[0].url;
+      console.log(imageUrl);
+      $(".image").attr("src", imageUrl);
     });
 };
 
@@ -23,34 +26,34 @@ var displayParks = function () {
   $(".searchBtn").on("click", function () {
     searchState = $("#state").val();
     console.log(searchState);
+
     getPark();
   });
 };
 displayParks();
 
-
 $(document).ready(() => {
-  // Initial data fetch
-  fetchWeather();
-})
+// Initial data fetch
+fetchWeather();
+});
 
 // easy way to reuse and build the url
+//
 /**
-* 
-* @param lat: latitude desired - default from defaultCoords
-* @param long: longitude desired - default from defaultCoords
-* @returns Promise<whatever the data is>
-*/
+ *
+ * @param lat: latitude desired - default from defaultCoords
+ * @param long: longitude desired - default from defaultCoords
+ * @returns Promise<whatever the data is>
+ */
 const getUrl = (lat, lon) => {
-  const urlLat = lat || defaultCoords.lat;
-  const urlLon = lon || defaultCoords.lon
-  return `${BASE_URL}?units=imperial&lat=${urlLat}&lon=${urlLon}&appid=${API_KEY}`;
-}
-
-const fetchWeather = async (lat, lon) => {
-  return await fetch(getUrl())
-      .then((res) => res.json())
-      .then((res) => $('.data-json').append(JSON.stringify(res, null, 2)))
-      .catch((err) => console.error('WEATHER ERR: ', err));
+const urlLat = lat || defaultCoords.lat;
+const urlLon = lon || defaultCoords.lon;
+return `${BASE_URL}?units=imperial&lat=${urlLat}&lon=${urlLon}&appid=${API_KEY}`;
 };
 
+const fetchWeather = async (lat, lon) => {
+return await fetch(getUrl())
+.then((res) => res.json())
+.then((res) => $(".data-json").append(JSON.stringify(res, null, 2)))
+.catch((err) => console.error("WEATHER ERR: ", err));
+};*
