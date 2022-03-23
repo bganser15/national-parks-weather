608eaaf8574ff15cb93d84bf2231c5c74ca326fc
Jaime Rivas <j.rivas4818@gmail.com>
2022-03-23 15:31:40 -0700

    made list of state parks to buttons and colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+const API_KEY = "efce923fdeaa8b3017a1d2da22a5ca95";
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall';
+const defaultCoords = { lat: 33.625274, lon: -112.218690 };
+
 //nps api key P7v76VxhVDmo5rOLwAyEnDqiIYeclDPZgcT0CdBK
 var searchState;
-var parkData;
 
-var getPark = function () {
+var getPark = function (searchTerm) {
   var nationalParksUrl =
     "https://developer.nps.gov/api/v1/parks?stateCode=" +
     searchState +
@@ -12,30 +15,42 @@ var getPark = function () {
       return response.json();
     })
     .then(function (data) {
-      parkData = data.data;
-      displayParks(parkData);
+      console.log(data);
     });
 };
 
-//displays parks in each state
-var displayParks = function (parks) {
-  //clears past search results
-  $(".placeholderContainer").empty();
-  for (var i = 0; i < parks.length; i++) {
-    var listPark = document.createElement("div");
-    listPark.textContent = parkData[i].fullName;
-    $(".placeholderContainer").append(listPark);
-  }
-};
-
-var getState = function () {
+var displayParks = function () {
   $(".searchBtn").on("click", function () {
     searchState = $("#state").val();
     console.log(searchState);
     getPark();
   });
 };
-getState();
+displayParks();
 
-//when state name is entered, a list of parks in the state is displayed in div
-//when park is clicked, it gets park n ame and returns data from the clicked event (using event.target) most likely
+$(document).ready(() => {
+  // Initial data fetch
+  fetchWeather();
+})
+
+// easy way to reuse and build the url
+/**
+* 
+* @param lat: latitude desired - default from defaultCoords
+* @param long: longitude desired - default from defaultCoords
+* @returns Promise<whatever the data is>
+*/
+const getUrl = (lat, lon) => {
+  const urlLat = lat || defaultCoords.lat;
+  const urlLon = lon || defaultCoords.lon
+  return `${BASE_URL}?units=imperial&lat=${urlLat}&lon=${urlLon}&appid=${API_KEY}`;
+}
+
+const fetchWeather = async (lat, lon) => {
+  return await fetch(getUrl())
+      .then((res) => res.json())
+      //.then((res) => $('.data-json').append(JSON.stringify(res, null, 2)))
+      //.catch((err) => console.error('WEATHER ERR: ', err));
+      .then((res) => $('.temp').append(res.current.temp))
+      .catch((err) => state['error'] = err);
+};
